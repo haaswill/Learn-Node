c2399365aa99546b2fd690bea3f2159f06c8abd4
Willian <haaswillian@gmail.com>
2017-05-16 23:20:01 -0300

    Finished 20th class

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -13,16 +13,40 @@ const storeSchema = new mongoose.Schema({
         type: String,
         trime: true
     },
-    tags: [String]
+    tags: [String],
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        coordinates: [{
+            type: Number,
+            required: 'You must supply coordinates!'
+        }],
+        address: {
+            type: String,
+            required: 'You must supply an address!'
+        }
+    },
+    photo: String
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
     if (!this.isModified('name')) {
         return next();
     }
     this.slug = slug(this.name);
+    // find other stores that have a slug of wes, wes-1, wes-2
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+    if (storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
     next();
-
     // TODO make more reisiliant so slugs are unique
 });
 
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -5,8 +5,24 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
+
+router.post('/add',
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id',
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.updateStore)
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
